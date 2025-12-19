aa16279492a06a1210d004d54d86abf302cb07cd
바키찬 <ckstmznf@naver.com>
2025-12-19 23:55:52 +0900

    Feat: 블록 종류 임시 몇개 만듬

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 
 #define MAP_WIDTH 10
 #define MAP_HEIGHT 20
+#define BLOCK_TYPE_COUNT 3
 
 #define BLOCK "█"
 #define BLANK "."
@@ -20,22 +21,79 @@ typedef struct Block {
     int shape[4][4];
 } Block;
 
+static Block BLOCK_TEMPLATE[BLOCK_TYPE_COUNT][4] = {
+        {
+                {1, 4, {
+                        {1},
+                        {1},
+                        {1},
+                        {1},
+                }},
+                {4, 1, {
+                        {1, 1, 1, 1},
+                }},
+                {1, 4, {
+                        {1},
+                        {1},
+                        {1},
+                        {1},
+                }},
+                {4, 1, {
+                        {1, 1, 1, 1},
+                }},
+        },
+        {
+                {3, 3, {
+                        {0, 0, 0},
+                        {0, 1, 0},
+                        {1, 1, 1}
+                }},
+                {2, 3, {
+                        {1, 0} ,
+                        {1, 1} ,
+                        {1, 0} ,
+                }},
+                {3, 2, {
+                        {1, 1, 1},
+                        {0, 1, 0}
+                }},
+                {2, 3, {
+                        {0, 1} ,
+                        {1, 1} ,
+                        {0, 1} ,
+                }},
+        },
+        {
+                {2, 2, {
+                        {1, 1},
+                        {1, 1},
+                }},
+                {2, 2, {
+                        {1, 1},
+                        {1, 1},
+                }},
+                {2, 2, {
+                        {1, 1},
+                        {1, 1},
+                }},
+                {2, 2, {
+                        {1, 1},
+                        {1, 1},
+                }},
+        },
+};
+
 int main() {
     SetConsoleOutputCP(CP_UTF8);
     initScreen();
 
     COORD position = {0, 0};
-    Block b = {
-            2, 2,
-            {
-                    {1, 1},
-                    {1, 1},
-            }
-    };
-
+    int block_rotate = 0;
 
     while (1) {
         clearScreen();
+
+        Block b = BLOCK_TEMPLATE[1][block_rotate];
 
         for (int y = 0; y < MAP_HEIGHT; y++) {
             for (int x = 0; x < MAP_WIDTH; x++) {
@@ -62,13 +120,17 @@ int main() {
         }
 
         if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
-            if(position.Y + b.height < MAP_HEIGHT) {
+            if (position.Y + b.height < MAP_HEIGHT) {
                 position.Y += 1;
             }
         }
+        if(GetAsyncKeyState(VK_UP) & 0x8000) {
+            block_rotate += 1;
+            block_rotate %= 4;
+        }
 
         flipScreen();
-        Sleep(33);
+        Sleep(50);
     }
     return 0;
 }
