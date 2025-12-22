96f8f33a1986e120bd52e1e8fcd6069092491951
바키찬 <ckstmznf@naver.com>
2025-12-22 22:26:59 +0900

    Feat: enter가 끝날때까지 대기

--- scene/gameResultScene.c
+++ scene/gameResultScene.c
@@ -12,10 +12,13 @@ void gameResultScene(int score) {
     while (1) {
         clearScreen();
         writeHorizontalCenter(10, COLOR_CYAN "=== GAME OVER ===" COLOR_RESET);
-        writeHorizontalCenter(11, fs("%s Score: %d %s", COLOR_GREEN, score, COLOR_RESET));
-        writeHorizontalCenter(13, fs("%s Enter Enter key for Restart %s", COLOR_YELLOW, COLOR_RESET));
+        writeHorizontalCenter(11, fs("%sScore: %d %s", COLOR_GREEN, score, COLOR_RESET));
+        writeHorizontalCenter(13, fs("%sPress Enter key for Restart %s", COLOR_YELLOW, COLOR_RESET));
 
         if(isKeyDowned(VK_RETURN)){
+            while (isKeyDown(VK_RETURN)) {
+                Sleep(10);
+            }
             return;
         }
 
