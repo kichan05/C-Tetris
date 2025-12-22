bbc8db64d694d11cf7d56300b42076ac26f0b6e7
바키찬 <ckstmznf@naver.com>
2025-12-22 22:27:34 +0900

    Feat: initScene에서 GetAsyncState 방식으로 변경

--- scene/initScene.c
+++ scene/initScene.c
@@ -18,21 +18,14 @@ int initScene() {
         writeScreen(0, 2, fs("%s2. Setting Game%s", selectMeniIndex == 1 ? COLOR_YELLOW : COLOR_RESET, COLOR_RESET));
         writeScreen(0, 3, fs("%s3. Exit%s", selectMeniIndex == 2 ? COLOR_YELLOW : COLOR_RESET, COLOR_RESET));
 
-        int key = getKeyPress();
-
-        switch (key) {
-            case KEY_DOWN:
-                if(selectMeniIndex + 1 <= 2) {
-                    selectMeniIndex += 1;
-                }
-                break;
-            case KEY_UP:
-                if(selectMeniIndex - 1 >= 0) {
-                    selectMeniIndex -= 1;
-                }
-                break;
-            case KEY_ENTER:
-                return selectMeniIndex;
+        if(isKeyDowned(VK_UP) && selectMeniIndex - 1 >= 0) {
+            selectMeniIndex -= 1;
+        }
+        else if(isKeyDowned(VK_DOWN) && selectMeniIndex + 1 <= 2) {
+            selectMeniIndex += 1;
+        }
+        else if(isKeyDowned(VK_RETURN)) {
+            return selectMeniIndex;
         }
 
         flipScreen();
